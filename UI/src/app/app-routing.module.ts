f849557a564d2104bd1396da585e6e4f8b917db7
wahidahmed <wahidahmed1992@gmail.com>
2024-05-17 10:58:45 +0600

    bootstrap

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./components/login/login.component').then(c => c.LoginComponent) }
+  , { path: 'signup', loadComponent: () => import('./components/signup/signup.component').then(c => c.SignupComponent) }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
